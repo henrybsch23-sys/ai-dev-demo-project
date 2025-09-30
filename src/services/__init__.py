bc4ef329c8c044e25295393ef4c7be9cf42d48c1
henrybsch23-sys <henrybsch.23@gmail.com>
2025-09-30 16:51:59 +0200

    Update __init__.py

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,1 +1,20 @@
+"""
+Reusable business logic used by API and (future) background workers.
+"""
+from .calculations import (
+    bmi,
+    bmr_mifflin_st_jeor,
+    tdee,
+    body_fat_navy,
+    daily_water_intake_l,
+)
+from .recommendations import simple_recommendations
 
+__all__ = [
+    "bmi",
+    "bmr_mifflin_st_jeor",
+    "tdee",
+    "body_fat_navy",
+    "daily_water_intake_l",
+    "simple_recommendations",
+]
